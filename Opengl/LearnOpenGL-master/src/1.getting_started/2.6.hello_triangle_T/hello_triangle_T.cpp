928b84ff19b2820f99d50431da8c2c7a3c9e082b
zwl1 <zwl1@meitu.com>
2024-06-18 23:42:50 +0800

    exercise2-6

--- Opengl/LearnOpenGL-master/src/1.getting_started/2.6.hello_triangle_T/hello_triangle_T.cpp
+++ Opengl/LearnOpenGL-master/src/1.getting_started/2.6.hello_triangle_T/hello_triangle_T.cpp
@@ -115,30 +115,112 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // 之后就是顶点数据
-    unsigned int VAO, VBO;
+    if (0)
+    {
 
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // 绑定VAO
-    glBindVertexArray(VAO);
-    // 绑定缓存数据
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // 复制用户数据
-    glBufferData(GL_ARRAY_BUFFER, sizeof(DoubleTranigleVertices), DoubleTranigleVertices, GL_STATIC_DRAW);
+		// 之后就是顶点数据
+		unsigned int VAO, VBO;
 
-    // 给程序说怎么理解数据
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // location 启动;
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		// 绑定VAO
+		glBindVertexArray(VAO);
+		// 绑定缓存数据
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		// 复制用户数据
+		glBufferData(GL_ARRAY_BUFFER, sizeof(DoubleTranigleVertices), DoubleTranigleVertices, GL_STATIC_DRAW);
+
+		// 给程序说怎么理解数据
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		// location 启动;
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+		
+		// 绘制两个三角形
+		while (!glfwWindowShouldClose(window))
+		{
+			processInput(window);
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			// draw out  first triangle
+			glUseProgram(shaderprogram);
+			glBindVertexArray(VAO); //标记着
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+		glDeleteProgram(shaderprogram);
+		glfwTerminate();
+		return 0;
+    }
+
+    // 使用两个VBO VAO
+    if (1)
+    {
+        float firstTriangle[] = {
+			-0.9f, -0.5f, 0.0f,  // left 
+			-0.0f, -0.5f, 0.0f,  // right
+			-0.45f, 0.5f, 0.0f,  // top 
+        };
+        float secondTriangle[] = {
+            0.0f, -0.5f, 0.0f,  // left
+            0.9f, -0.5f, 0.0f,  // right
+            0.45f, 0.5f, 0.0f   // top 
+        };
+        // 一个VAO绑定就是一个顶点数据
+        unsigned int VBOs[2], VAOs[2];
+        glGenVertexArrays(2, VAOs);
+        glGenBuffers(2,VBOs);
+
+        // 然后每次绑定信息 先绑定顶点数据，在绑定附加的数据
+        glBindVertexArray(VAOs[0]);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+        // 塞入用户指定的数据
+        glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Vertex attributes stay the same
+        glEnableVertexAttribArray(0);
+
+
+        glBindVertexArray(VAOs[1]);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+        // 塞入用户指定的数据
+        glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); // because the vertex data is tightly packed we can also specify 0 as the vertex attribute's stride to let OpenGL figure it out
+        glEnableVertexAttribArray(0);
+
+
+        // 绘制
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
+
+		while (!glfwWindowShouldClose(window))
+		{
+			processInput(window);
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			// draw out  first triangle
+			glUseProgram(shaderprogram);
+			glBindVertexArray(VAOs[0]); //标记着
+			glDrawArrays(GL_TRIANGLES, 0, 3);   
+            // 绑定完之后就可以那这个顶点进行绘制
+			glBindVertexArray(VAOs[1]); //标记着
+			glDrawArrays(GL_TRIANGLES, 0, 3);   
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+		glDeleteVertexArrays(2, VAOs);
+		glDeleteBuffers(2, VBOs);
+		glDeleteProgram(shaderprogram);
+		glfwTerminate();
+		return 0;
+    }
 
 
 
-
-    glfwTerminate();
-    return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
